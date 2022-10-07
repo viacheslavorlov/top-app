eeac78ea86617a4ad6dda6b8dac96bbc8964f200
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-10-07 13:44:34 +0300

    start to figma

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import type {AppProps} from 'next/app';
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>MyTop - лучший топ</title>
+                <link  rel="icon" href="/favicon2.ico"/>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
